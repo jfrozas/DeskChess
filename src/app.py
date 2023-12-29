d11ea496a44639c8f3fa0d1efb7131029ebb5483
jfrozas <j.frozas@udc.es>
2023-12-30 00:23:43 +0100

    Started the tests

--- src/app.py
+++ src/app.py
@@ -6,11 +6,11 @@ import chess.pgn
 import chess.engine
 import os
 import sys
+sys.path.append('../DeskChess')
+
 from PIL import ImageTk, Image
 from datetime import datetime
 import io
-
-from view_game_aux import draw_board
 
 class ChessApp:
     
@@ -520,6 +520,34 @@ class ChessApp:
         if self.engine:
             self.engine.close()
         self.root.destroy()
+
+def draw_board(canvas, board, script_dir, images):
+    
+    canvas.delete("all")
+    
+    for i in range(8):
+        for j in range(8):
+            color = 'white' if (i+j)%2 == 0 else 'gray'
+            canvas.create_rectangle(i*50, j*50, (i+1)*50, (j+1)*50, fill=color)
+
+    for i in range(8):
+        for j in range(8):
+            piece = board.piece_at(chess.square(i, 7-j))
+            if piece is not None:
+                if piece.color == chess.WHITE:
+                    filename = os.path.join(script_dir, "piezas", f"white_{str(piece)}.png")
+                else:
+                    filename = os.path.join(script_dir, "piezas", f"black_{str(piece)}.png")
+                
+                try:
+                    image = Image.open(filename)
+                    image = image.resize((50, 50), Image.ANTIALIAS)
+                    photo = ImageTk.PhotoImage(image)
+                    images[(i, j)] = photo
+                    canvas.create_image(i*50+25, j*50+25, image=photo)
+
+                except Exception as e:
+                    print(f"Error loading image: {e}") 
 
 def main():
     window = tk.Tk()
